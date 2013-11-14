ec304a0aa79e7d681949348d60f34deafcb9ce63
Dmytriy Manzhosov <manzhosov.d@dbbest.com>
2013-11-14 17:02:42 +0200

    initial commit

---
+++ src/com/dbbest/framework/sort/abstraction/AbstractSortMode.java
@@ -0,0 +1,9 @@
+package com.dbbest.framework.sort.abstraction;
+
+public abstract class AbstractSortMode<T, R> implements ISortMode<T, R> {
+	protected ISortParameter<T> sortType = null;
+
+	public void setSortType(ISortParameter<T> type) {
+		this.sortType = type;
+	}
+}
---
+++ src/com/dbbest/framework/system/OutOfMemoryException.java
@@ -0,0 +1,14 @@
+package com.dbbest.framework.system;
+
+/**
+ * Created by Tikhonenko.S on 04.10.13.
+ */
+public class OutOfMemoryException extends Exception{
+    public OutOfMemoryException(String message, OutOfMemoryError cause) {
+        super(message, cause);
+    }
+
+    public OutOfMemoryException(OutOfMemoryError cause) {
+        super(cause);
+    }
+}
