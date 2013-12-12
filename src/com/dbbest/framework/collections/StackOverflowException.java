211bf46f94c9970c0b935daa9d6a12729e7093e6
Semyon Tikhonenko <Tikhonenko.S@dbbest.com>
2013-12-12 13:56:47 +0200

    Stack added

---
+++ src/com/dbbest/framework/collections/StackOverflowException.java
@@ -0,0 +1,23 @@
+package com.dbbest.framework.collections;
+
+/**
+ * User: Tikhonenko.S
+ * Date: 12.12.13
+ * Time: 13:35
+ */
+public class StackOverflowException extends RuntimeException{
+    public StackOverflowException() {
+    }
+
+    public StackOverflowException(String message) {
+        super(message);
+    }
+
+    public StackOverflowException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StackOverflowException(Throwable cause) {
+        super(cause);
+    }
+}
