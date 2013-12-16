ee8dbb0b171e5b91e79b7fe1c9363ce408cbe3d4
Semyon Tikhonenko <Tikhonenko.S@dbbest.com>
2013-12-16 18:37:30 +0200

    CollectionUtils.java updated, windows build .jar script

---
+++ src/com/dbbest/framework/predicates/InstanceOfPredicate.java
@@ -0,0 +1,21 @@
+package com.dbbest.framework.predicates;
+
+import com.dbbest.framework.Predicate;
+
+/**
+ * User: Tikhonenko.S
+ * Date: 16.12.13
+ * Time: 14:23
+ */
+public class InstanceOfPredicate<T> implements Predicate<T>{
+    private Class aClass;
+
+    public InstanceOfPredicate(Class<? extends T> aClass) {
+        this.aClass = aClass;
+    }
+
+    @Override
+    public boolean check(T item) {
+        return aClass.isAssignableFrom(item.getClass());
+    }
+}
