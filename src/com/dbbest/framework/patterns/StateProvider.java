be0e5b8ce41fbfd94202dab1065418d0de7ab1a8
Semyon Tikhonenko <Tikhonenko.S@dbbest.com>
2013-12-26 16:23:21 +0200

    ObjectsStatesManager.java

---
+++ src/com/dbbest/framework/patterns/StateProvider.java
@@ -0,0 +1,11 @@
+package com.dbbest.framework.patterns;
+
+/**
+ * User: Tikhonenko.S
+ * Date: 26.12.13
+ * Time: 15:00
+ */
+public interface StateProvider<T, State> {
+    State getState(T object);
+    void restoreState(T object, State state);
+}
---
+++ src/com/dbbest/framework/patterns/StateRestorer.java
@@ -0,0 +1,10 @@
+package com.dbbest.framework.patterns;
+
+/**
+ * User: Tikhonenko.S
+ * Date: 26.12.13
+ * Time: 15:03
+ */
+public interface StateRestorer {
+    void restore();
+}
