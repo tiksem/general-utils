3bd5ef91a7f1a896dfb50f678136edec29f691da
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2014-11-13 21:53:16 +0200

    Reflection clone

--- src/com/utils/framework/Reflection.java
+++ src/com/utils/framework/Reflection.java
@@ -1,10 +1,13 @@
 package com.utils.framework;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.net.URL;
 import java.util.*;
 
 /**
@@ -170,5 +173,84 @@ public final class Reflection {
                 return false;
             }
         });
+    }
+
+    private final static char DOT = '.';
+    private final static char SLASH = '/';
+    private final static String CLASS_SUFFIX = ".class";
+    private final static String BAD_PACKAGE_ERROR = "Unable to get resources from path '%s'. Are you sure the given '%s' package exists?";
+
+    public static String[] classesCanonicalAsNamesStringArray(Collection<Class<?>> classes) {
+        String[] result = new String[classes.size()];
+        int index = 0;
+
+        for(Class aClass : classes){
+            result[index++] = aClass.getCanonicalName();
+        }
+
+        return result;
+    }
+
+    public static List<Class<?>> findClassesInPackage(final String scannedPackage) {
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        final String scannedPath = scannedPackage.replace(DOT, SLASH);
+        final Enumeration<URL> resources;
+        try {
+            resources = classLoader.getResources(scannedPath);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(String.format(BAD_PACKAGE_ERROR, scannedPath, scannedPackage), e);
+        }
+
+        final List<Class<?>> classes = new ArrayList<Class<?>>();
+        while (resources.hasMoreElements()) {
+            final File file = new File(resources.nextElement().getFile());
+            classes.addAll(findClassesInPackage(file, scannedPackage));
+        }
+
+        return classes;
+    }
+
+    private static List<Class<?>> findClassesInPackage(final File file, final String scannedPackage) {
+        final List<Class<?>> classes = new LinkedList<Class<?>>();
+        final String resource = scannedPackage + DOT + file.getName();
+        if (file.isDirectory()) {
+            for (File nestedFile : file.listFiles()) {
+                classes.addAll(findClassesInPackage(nestedFile, scannedPackage));
+            }
+        } else if (resource.endsWith(CLASS_SUFFIX)) {
+            final int beginIndex = 0;
+            final int endIndex = resource.length() - CLASS_SUFFIX.length();
+            final String className = resource.substring(beginIndex, endIndex);
+            try {
+                classes.add(Class.forName(className));
+            } catch (ClassNotFoundException ignore) {
+            }
+        }
+        return classes;
+    }
+
+    public static <T> T cloneObject(T object) {
+        try {
+            T result = (T) object.getClass().newInstance();
+            List<Field> fields = getAllFields(object);
+            for(Field field : fields){
+                if (!Modifier.isStatic(field.getModifiers())) {
+                    Object value = getValueOfField(object, field);
+                    setValueOfField(result, field, value);
+                }
+            }
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> List<T> cloneObjects(Iterable<? extends T> objects) {
+        List<T> result = new ArrayList<T>();
+        for(T object : objects) {
+            result.add(cloneObject(object));
+        }
+
+        return result;
     }
 }
