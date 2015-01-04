e62f0c22c5ba853a701fe19544b2680822a6f164
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-01-04 16:06:00 +0200

    removeDirectory

--- src/com/utils/framework/io/IOUtilities.java
+++ src/com/utils/framework/io/IOUtilities.java
@@ -228,20 +228,35 @@ public final class IOUtilities {
         }
     }
 
-    public static boolean deleteDirectory(File directory) {
-        if(directory.exists()){
-            File[] files = directory.listFiles();
-            if(null != files){
-                for (File file : files) {
-                    if (file.isDirectory()) {
-                        deleteDirectory(file);
-                    } else {
-                        file.delete();
-                    }
-                }
+    public static boolean removeDirectory(File directory) {
+        if (directory == null)
+            return false;
+        if (!directory.exists())
+            return true;
+        if (!directory.isDirectory())
+            return false;
 
+        String[] list = directory.list();
+
+        // Some JVMs return null for File.list() when the
+        // directory is empty.
+        if (list != null) {
+            for (int i = 0; i < list.length; i++) {
+                File entry = new File(directory, list[i]);
+
+                if (entry.isDirectory())
+                {
+                    if (!removeDirectory(entry))
+                        return false;
+                }
+                else
+                {
+                    if (!entry.delete())
+                        return false;
+                }
             }
         }
-        return(directory.delete());
+
+        return directory.delete();
     }
 }
