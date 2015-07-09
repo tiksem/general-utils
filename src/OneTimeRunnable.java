8e1e945c14ed403c6bbf4b0177c0c1712025ef7c
Semyon Tikhonenko <semyontikhonenko@gmail.com>
2015-07-09 16:24:03 +0300

    OnTimeRunnable

---
+++ src/OneTimeRunnable.java
@@ -0,0 +1,15 @@
+/**
+ * Created by CM on 7/9/2015.
+ */
+public abstract class OneTimeRunnable implements Runnable {
+    private boolean executed = false;
+    protected abstract void doAction();
+
+    @Override
+    public void run() {
+        if (!executed) {
+            doAction();
+            executed = true;
+        }
+    }
+}
