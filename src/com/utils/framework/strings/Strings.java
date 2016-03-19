1b6dc4850ea50438a95a9c21dcb49719773179e8
Semen Tykhonenko <stykhonenko@cogniance.com>
2016-03-19 17:13:32 +0200

    Strings.toLowerCase fixed

--- src/com/utils/framework/strings/Strings.java
+++ src/com/utils/framework/strings/Strings.java
@@ -436,7 +436,7 @@ public class Strings {
         int length = source.length;
         String[] result = new String[length];
         for (int i = 0; i < length; i++) {
-            result[i] = source[i];
+            result[i] = source[i].toLowerCase();
         }
 
         return result;
